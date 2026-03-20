0a351b7ce6808b4a6ffaf2e4d2d7b12cd9977ccd
meti-guda <metigmerd@gmail.com>
2026-03-20 17:35:35 +0300

    feat: add basic row counter route

---
+++ knit-forge/app/row-counter/page.tsx
@@ -0,0 +1,7 @@
+export default function RowCounterPage() {
+     return (
+          <main className="min-h-screen flex items-center justify-center">
+               <h1>Row Counter</h1>
+          </main>
+     );
+}
